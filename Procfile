e2461cfba839d639197a86724b1e72561e9a1a85
Andrew Weil <weilandrew@icloud.com>
2024-02-17 15:14:54 -0500

    yes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn tau-site:app
+web: gunicorn app:app
--- test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-number_table = pd.read_csv("number-sheet.csv")
-for index, row in number_table.iterrows():
-    print(row['Name'], row['Number'])
